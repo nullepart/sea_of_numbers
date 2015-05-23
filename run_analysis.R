0cf4162d8457c20406483e3de66ff770c504add4
nullepart <fpdube@yahoo.ca>
2015-05-23 18:29:36 +0200

    Nettoyé le script.

--- run_analysis.R
+++ run_analysis.R
@@ -23,36 +23,43 @@ train.sub <- read.table("./data/raw/train/subject_train.txt", header = F)
 train.acti <- read.table("./data/raw/train/y_train.txt", header = F)
 train.data <- read.table("./data/raw/train/X_train.txt", header = F)
 df.train <- cbind(train.sub, train.acti, train.data)
-#rm(train.sub) ; rm(train.acti); rm(train.data) # clean up memory
+rm(train.sub) ; rm(train.acti); rm(train.data) # clean up memory
 
+# Joining together the subject, activity nb, and data for train & test
 test.sub <- read.table("./data/raw/test/subject_test.txt", header = F)
 test.acti <- read.table("./data/raw/test/y_test.txt", header = F)
 test.data <- read.table("./data/raw/test/X_test.txt", header = F)
 df.test <- cbind(test.sub, test.acti, test.data)
+rm(test.sub) ; rm(test.acti); rm(test.data) # clean up memory
 
 df.total <- rbind(df.test, df.train)
+
+# Adding the proper column headers (subj, acti, features)
+# N.B. normally part of STEP IV, but done here
 
 var1 <- c("subject", "activity.n")
 var2 <- read.table("./data/raw/features.txt", header = F)
 var <- c(var1, as.character(var2[, 2]))
 rm(var1) ; rm(var2)
 
-names(df.total) <- var
+# But these column names are not entirely legal in R
+# We will remove all parentheses and hyphens from the names;
+# then, remove all ".." and "..." introduced by make.names
+var3 <- make.names(var)
+var3 <- gsub(pattern = "...", replacement = ".", x = var3, fixed = T)
+var3 <- gsub(pattern = "..", replacement = "", x = var3, fixed = T)
+
+# Passing out these new column names into a new data.table
+names(df.total) <- var3
 dt.total <- data.table(df.total)
 setkey(x = dt.total, activity.n)
 
-
-#rm(test.sub) ; rm(test.acti); rm(test.data) # clean up memory
-#rm(df.test); rm(df.total); rm(df.train)
+rm(df.test); rm(df.total); rm(df.train)
 
 
 # STEP 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
-  ### This works. It is just very complicated.
-  # q <- quote(c(grep("subject", names(dt.total)), grep("activity.n", names(dt.total)), grep("std", names(dt.total)), grep("mean", names(dt.total))))
-  # dt <-dt.total[, eval(q), with = F]
-
-### This is easier: using the subset function on a data.table
+### Using the subset function on a data.table
 cols <- c("subject", "activity.n", grep("mean|std", names(dt.total), value = T))
 dt <- subset(dt.total, select = cols)
 
@@ -63,38 +70,23 @@ acti <- data.table(read.table(file = "./data/raw/activity_labels.txt",
 setnames(acti, c("V1", "V2"), c("activity.n", "activity"))
 setkey(acti, activity.n)
 
-dt <- dt[acti] # Joins acti and dt using their common key
+# Joins acti and dt using their common key
+dt <- dt[acti] 
 n.col <- dim(dt)[2]
+
+# To put activity at thebeginning of the table
 setcolorder(dt, c("subject", "activity.n", 
                   "activity", 
-                  names(dt)[4:n.col-1])) # To put activity at the
-                                         # beginning of the table
+                  names(dt)[4:n.col-1])) 
 
 
 # STEP 4. Appropriately labels the data set with descriptive variable names. 
-
 # Already done as part of STEP 1. But let's clean up.
 
 setkey(dt, subject)
 dt[, activity.n := NULL] # Deletes this column
 
+
 # STEP 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
 
 dt2 <- dt[, lapply(.SD, mean), by=list(activity, subject)]
-
-
-##### Carré de sable
-
-View(test.sub[1:10, ])
-View(test.acti[1:10, ])
-View(test.data[1:10, ])
-
-temp <- read.table("./data/raw/train/Inertial Signals/body_acc_x_train.txt", header = F)
-tempdf <- temp[1:5, ]
-View(tempdf)
-dim(temp)
-
-temp2 <- read.table("./data/raw/train/X_train.txt", header = F)
-df2 <- temp2[1:5, ]
-View(df2)
-dim(temp2)
